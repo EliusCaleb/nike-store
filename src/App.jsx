8f979d768dbabd494e308f7f816fe4b82f896a3e
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-06 09:51:01 +0300

    storing the data and image in asset and data folder

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Home, }from './components/Home'
+
+function App() {
+  return (
+    <div>
+      <main>
+      <Home/>
+      </main>
+       
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <div>
+        <div></div>
+        <div>
+           <div>
+               <h1></h1>
+               <h1></h1>
+               <button></button>
+               <div></div>
+               <div></div>
+           </div>
+           <div>
+            <img  className='' src='' alt='home-img/img'/>
+           </div>
+        </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Home
