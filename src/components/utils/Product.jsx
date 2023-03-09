7f3c51c40c7d36a8c49940221da0769c59089542
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-09 16:19:36 +0300

    made product dynamic

--- src/components/utils/Product.jsx
+++ src/components/utils/Product.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { ShoppingBagIcon, StarIcon } from "@heroicons/react/24/solid";
 
-function Product({ id, color, shadow, title, text, img, btn, rating, price }) {
+function Product({ifExists, id, color, shadow, title, text, img, btn, rating, price }) {
   return (
     <>
-      <div className={` relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center rounded-xl px-5 py-4  transition-all duration-700 ease-in-out w-full hover:scale-105`}>
-        <div className="grid items-center justify-items-center ">
+      <div className={` relative bg-gradient-to-b ${color} ${shadow} grid items-center     ${ifExists? 'justify-items-start': 'justify-items-center'}  rounded-xl px-5 py-4  transition-all duration-700 ease-in-out w-full hover:scale-105`}>
+        <div className={`grid items-center ${ifExists ?'justify-items-start': 'justify-items-center'}`}>
           <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow">{title}</h1>
           <p className="text-slate-200 filter drop-shadow text-base md:text-sm font-normal">{text}</p>
 
@@ -28,9 +28,16 @@ function Product({ id, color, shadow, title, text, img, btn, rating, price }) {
             </button>
           </div>
         </div>
-        <div>
-          <img src={img} alt="img/item-img" 
-          className="h-36 w-64 trasitions-theme hover:-rotate-12"/>
+        <div className={`flex items-center ${ifExists ? 'absolute top-5 right-1': 'justify-center'}`}>
+        <img
+            src={img}
+            alt={`img/item-img/${id}`}
+            className={`transitions-theme hover:-rotate-12 ${
+              ifExists
+                ? "h-auto w-64 lg:w-56 md:w-48 -rotate-[35deg]"
+                : "h-36 w-64"
+            }`}
+          />
         </div>
       </div>
     </>
