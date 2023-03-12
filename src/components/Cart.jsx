005545eb89d11508517ba2417cc6a6b6cab20433
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-12 20:04:38 +0300

    added to total to the cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,42 +1,83 @@
-import React from 'react'
+import React, {useEffect}from 'react'
 import { useDispatch,useSelector } from 'react-redux'
-import { selectCartItem, selectCartState,setCloseCart } from '../app/CartSlice'
+import {
+  selectCartItem,
+  selectCartState,
+  selectTotalAmount,
+  selectTotalQTY,
+  setClearCartItem,
+  setCloseCart,
+  setGetTotals
+} from "../app/CartSlice.js";
 import CartCount from './cart/CartCount'
 import CartEmpty from './cart/CartEmpty'
 import CartItem from './cart/CartItem'
 
 function Cart() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const ifCartState = useSelector(selectCartState);
   const cartItem = useSelector(selectCartItem);
-  console.log(cartItem)
-  const onCartToggle =()=>{
-         dispatch(setCloseCart({
-            cartState:false
-         }))
+  const totalAmount = useSelector(selectTotalAmount);
+  const totalQTY = useSelector(selectTotalQTY);
+  
+  // console.log(cartItems)
+
+  useEffect(() => {
+    dispatch(setGetTotals())
+  },[cartItem, dispatch])
+  
+  const onCartToggle = () => {
+    dispatch(
+      setCloseCart({
+        cartState: false,
+      })
+    );
+  };
+
+  const onClearCartItem = () => {
+    dispatch(setClearCartItem())
   }
+  
   return (
-    <> 
-    <div className={`fixed blur-effect-theme top-0 right-0 left-0 w-full  h-screen opacity-100 z-[250] ${ifCartState ? 'opacity-100 visible translate-x-0':'opacity-0 invisible translate-x-8'}`}>
-        <div className={`blur-effect-theme h-screen max-w-xl w-full absolute right-0`}>
-            <CartCount onCartToggle={onCartToggle}/>
-            {cartItem.length === 0 ? <CartEmpty/> : 
-            
-            <div>
-              <div>
-                {cartItem?.map((item,index)=>(
-                  <CartItem key={index} item={item} />
-                )
-                )}
+    <>
+      <div
+        className={`fixed top-0 left-0 right-0 bottom-0 blur-effect-theme duration-500 w-full h-screen opacity-100 z-[250] ${
+          ifCartState
+            ? "opacity-100 visible translate-x-0"
+            : "opacity-0 invisible translate-x-8"
+        }`}
+      >
+        <div
+          className={`blur-effect-theme duration-500 h-screen max-w-xl w-full absolute right-0 ${
+            ifCartState
+              ? "opacity-100 visible translate-x-0"
+              : "opacity-0 invisible translate-x-8"
+          }`}
+        >
+          <CartCount totalQTY={totalQTY} onCartToggle={onCartToggle} onClearCartItem={onClearCartItem} />
+          {cartItem?.length === 0 ? <CartEmpty onCartToggle={onCartToggle} /> : <div>
+            <div className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] scroll-smooth scroll-hidden py-3">
+              {cartItem?.map((item, i) => (
+                <CartItem key={i} item={item} />
+              ))}
+            </div>
+
+            <div className="fixed bottom-0 bg-white w-full px-5 py-2 grid items-center">
+              <div className="flex items-center justify-between">
+                <h1 className="text-base font-semibold uppercase">SubTotal</h1>
+                <h1 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">${totalAmount}</h1>
+              </div>
+              <div className="grid items-center gap-2">
+                <p className="text-sm font-medium text-center">Taxes and Shipping Will Calculate At Shipping</p>
+                <button type="button" className="button-theme bg-theme-cart text-white">Check Out</button>
               </div>
             </div>
-            
-            }
+
+          </div>}
         </div>
-    </div>
-
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
