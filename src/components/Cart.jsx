ba4440c1474a0aec6c8f84e43325d09976095de0
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-13 06:49:53 +0300

    added clear items to cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect}from 'react'
-import { useDispatch,useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   selectCartItem,
   selectCartState,
@@ -7,11 +7,11 @@ import {
   selectTotalQTY,
   setClearCartItem,
   setCloseCart,
-  setGetTotals
+  setGetTotals,
 } from "../app/CartSlice.js";
-import CartCount from './cart/CartCount'
-import CartEmpty from './cart/CartEmpty'
-import CartItem from './cart/CartItem'
+import CartCount from "./cart/CartCount";
+import CartEmpty from "./cart/CartEmpty";
+import CartItem from "./cart/CartItem";
 
 function Cart() {
   const dispatch = useDispatch();
@@ -19,13 +19,13 @@ function Cart() {
   const cartItem = useSelector(selectCartItem);
   const totalAmount = useSelector(selectTotalAmount);
   const totalQTY = useSelector(selectTotalQTY);
-  
+
   // console.log(cartItems)
 
   useEffect(() => {
-    dispatch(setGetTotals())
-  },[cartItem, dispatch])
-  
+    dispatch(setGetTotals());
+  }, [cartItem, dispatch]);
+
   const onCartToggle = () => {
     dispatch(
       setCloseCart({
@@ -35,9 +35,9 @@ function Cart() {
   };
 
   const onClearCartItem = () => {
-    dispatch(setClearCartItem())
-  }
-  
+    dispatch(setClearCartItem());
+  };
+
   return (
     <>
       <div
@@ -54,30 +54,48 @@ function Cart() {
               : "opacity-0 invisible translate-x-8"
           }`}
         >
-          <CartCount totalQTY={totalQTY} onCartToggle={onCartToggle} onClearCartItem={onClearCartItem} />
-          {cartItem?.length === 0 ? <CartEmpty onCartToggle={onCartToggle} /> : <div>
-            <div className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] scroll-smooth scroll-hidden py-3">
-              {cartItem?.map((item, i) => (
-                <CartItem key={i} item={item} />
-              ))}
-            </div>
-
-            <div className="fixed bottom-0 bg-white w-full px-5 py-2 grid items-center">
-              <div className="flex items-center justify-between">
-                <h1 className="text-base font-semibold uppercase">SubTotal</h1>
-                <h1 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">${totalAmount}</h1>
+          <CartCount
+            totalQTY={totalQTY}
+            onCartToggle={onCartToggle}
+            onClearCartItem={onClearCartItem}
+          />
+          {cartItem?.length === 0 ? (
+            <CartEmpty onCartToggle={onCartToggle} />
+          ) : (
+            <div>
+              <div className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] scroll-smooth scroll-hidden py-3">
+                {cartItem?.map((item, i) => (
+                  <CartItem key={i} item={item} />
+                ))}
               </div>
-              <div className="grid items-center gap-2">
-                <p className="text-sm font-medium text-center">Taxes and Shipping Will Calculate At Shipping</p>
-                <button type="button" className="button-theme bg-theme-cart text-white">Check Out</button>
+
+              <div className="fixed bottom-0 bg-white w-full px-5 py-2 grid items-center">
+                <div className="flex items-center justify-between">
+                  <h1 className="text-base font-semibold uppercase">
+                    SubTotal
+                  </h1>
+                  <h1 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">
+                    ${totalAmount}
+                  </h1>
+                </div>
+                <div className="grid items-center gap-2">
+                  <p className="text-sm font-medium text-center">
+                    Taxes and Shipping Will Calculate At Shipping
+                  </p>
+                  <button
+                    type="button"
+                    className="button-theme bg-theme-cart text-white"
+                  >
+                    Check Out
+                  </button>
+                </div>
               </div>
             </div>
-
-          </div>}
+          )}
         </div>
       </div>
     </>
   );
-};
+}
 
 export default Cart;
